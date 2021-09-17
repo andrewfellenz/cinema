610ffcb001a677c2834af08785b1661820dea39d
Andrew Fellenz <andrewfellenz@gmail.com>
2021-09-17 13:55:06 -0500

    fixed the time pipe!

--- cinema/src/app/pipes/time.pipe.ts
+++ cinema/src/app/pipes/time.pipe.ts
@@ -14,7 +14,7 @@ export class TimeFormat implements PipeTransform {
     } else if (hour > 12) {
       hour = hour - 12;
     }
-    hour = parseInt(hour) <= 10 ? hour : `${hour[1]}`;
+    hour = parseInt(hour) <= 10 && hour.length == 2 ? hour[1] : hour;
     min = (min + "").length == 1 ? `0${min}` : min;
     return `${hour}:${min} ${part}`;
   }
